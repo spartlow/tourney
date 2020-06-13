d2f6f218c2b6c623c99adbe68e44db6dc0222e18
Steve Partlow <stevenpartlow@gmail.com>
2020-06-12 21:02:21 -0400

    historical games

---
+++ games.py
@@ -0,0 +1,59 @@
+year2018 = '''
+Steve Brian over Gene Dan
+Gene Jim over Steve Brian Rae
+Brian Sam over Steve Jim
+Steve Gene over Brian Sam
+Brian Jermaine over Steve Sam
+Brian Gene Sam over Steve Jermaine
+Brian Jermaine over Gene Jim
+Gene Jermaine over Brian Jim
+'''
+
+year2017 = '''
+Steve Brian Dave over Gene Matt Sam
+Steve Brian Dave over Jim Matt Sam
+Jim Dave Matt over Steve Gene Sam
+Steve Brian Sam over Gene Jim Matt
+Steve Jim Matt over Brian Dave Sam
+Steve Gene over Brian Sam
+'''
+
+year2016 = '''
+Steve Brian Gene over Rachel Jermaine Natalie Matt
+Matt Dave Jim over Steve Brian Gene
+Jermaine Dave Jim over Steve Brian Matt
+Steve Gene Jermaine over Brian Natalie Dan
+Steve Jermaine Matt over Brian Natalie Dave Victoria
+Steve Natalie Matt over Brian Jermaine
+Brian Dave JimS over Gene Jim Dan
+Brian Jim Dan over Gene Dave JimS
+'''
+
+year2015 = '''
+Gene Sam Steve over Brian Jack Seth
+Matt Seth Steve over Brian Jack Sam
+Dave Gene Sam over Matt Seth Steve
+Brian Dave Sam over Jim Matt Steve
+Brian Matt Steve over Gene Jim Sam
+Brian Gene Jim over Matt Sam Steve
+Dave Steve over Brian Sam
+Brian Gene Seth Steve over Dave Jim Matt Sam
+Jim Matt Seth Steve over Brian Dave Gene Sam
+Brian Jack Steve over Jim Sam Seth
+Jack Jim over Brian Gene
+Gene Jim over Brian Jack 
+'''
+
+year2014 = '''
+Steve Brian Seth over Matt Dan Jack
+Steve Jack Dave over Seth Jim Sam
+Dave Jim Sam over Steve Brian Matt
+Brian Matt Sam over Steve Dave Gene
+Brian Dan Jack over Steve Matt Jim
+Brian Dave Sam over Steve Matt Jim
+Dan Dave Jim over Brian Sam Victoria
+Matt Dave over Steve Brian Sam
+Steve Sam over Brian Seth
+Steve Brian over Matt Seth Sam
+'''
+
