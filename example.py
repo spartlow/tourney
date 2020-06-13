dcb4ab5847166de1cbe32652c119f52b866ad1b9
Steve Partlow <stevenpartlow@gmail.com>
2020-06-12 22:38:26 -0400

    print all tourneys

--- example.py
+++ example.py
@@ -1,4 +1,6 @@
 import tourney
+import tournaments
+
 t = tourney.Tourney()
 games_string = '''
 Steve Dave Jim over Brian Gene Shannon
@@ -8,3 +10,12 @@ Dave Gene Seth over Brian Jennn
 t.parse_games(games_string)
 df = tourney.get_scores_per_game_dataframe(t, tourney.ELO)
 print(df.head(3))
+
+for key in tournaments.tournaments:
+    print(key+":")
+    t = tourney.Tourney()
+    t.parse_games(tournaments.tournaments[key])
+    df = tourney.get_scores_per_game_dataframe(t, tourney.ELO)
+    print(df)
+    #print(df.drop(columns="Game Summaries").iloc[-1])
+    print(df.drop(columns="Game Summaries").iloc[-1].sort_values())
